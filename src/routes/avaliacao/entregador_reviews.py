1d65f660c17b64c56f6b4bdd543cac13cdb60618
diego00711 <ddiegowweber@gmail.com>
2025-08-30 09:32:05 -0300

    Fix import path for helper functions

--- src/routes/avaliacao/entregador_reviews.py
+++ src/routes/avaliacao/entregador_reviews.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from ..utils.helpers import get_db_connection, get_user_id_from_token
+from src.utils.helpers import get_db_connection, get_user_id_from_token
 
 entregador_reviews_bp = Blueprint('entregador_reviews_bp', __name__)
 
